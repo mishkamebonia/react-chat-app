60847bf72b3b28a6f0ae13dada027f329b7c1462
mishkamebonia <mishkas.mebonia@gmail.com>
2024-01-22 18:38:21 +0400

    created firebase db

---
+++ src/config/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyALeTWmmCO-WypNYiEvWgtzmCH8AIgSQPA",
+  authDomain: "fir-chat-app-a2fa1.firebaseapp.com",
+  projectId: "fir-chat-app-a2fa1",
+  storageBucket: "fir-chat-app-a2fa1.appspot.com",
+  messagingSenderId: "696157834623",
+  appId: "1:696157834623:web:38e7254c2e43c595131e4a"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
