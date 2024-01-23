54fe1d4f189b3107618af3e457fe39b1105052fb
mishkamebonia <mishkas.mebonia@gmail.com>
2024-01-23 21:25:40 +0400

    changed main page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,8 @@
 import { User } from "firebase/auth";
 import NavBar from "../components/NavBar";
 
+import { TextField, Box, Grid, Button } from "@mui/material";
+
 interface Props {
   user: User | null;
 }
@@ -12,7 +14,47 @@ const MainPage = (props: Props) => {
   return (
     <div>
       <NavBar />
-      <h1>main page</h1>
+      <Grid
+        sx={{ height: "94vh" }}
+        container
+        direction="row"
+        justifyContent="space-between"
+        alignItems="stretch"
+      >
+        <Grid item xs={2} sx={{ bgcolor: "#B9B4C7" }}>
+          <Box sx={{ p: 4 }}>12</Box>
+          <Box sx={{ p: 4 }}> 34</Box>
+        </Grid>
+        <Grid item xs={10} sx={{ bgcolor: "#fff" }}>
+          <Grid
+            container
+            direction="column"
+            justifyContent="space-between"
+            sx={{ p: 4, height: "100%" }}
+          >
+            <Grid item xs={5}>
+              text
+            </Grid>
+            <Grid item>
+              <Grid container spacing={2}>
+                <Grid item xs={10}>
+                  <TextField
+                    size="small"
+                    fullWidth
+                    placeholder="Type a message"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={2}>
+                  <Button fullWidth color="primary" variant="contained">
+                    Send
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 };
