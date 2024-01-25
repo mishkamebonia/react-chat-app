a1f7c3a029c23d17bc1969ba24a234ca19cc2847
mishkamebonia <mishkas.mebonia@gmail.com>
2024-01-25 16:36:16 +0400

    created auth provider

---
+++ src/providers/auth.tsx
@@ -0,0 +1,69 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../config/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { signOut } from "firebase/auth";
+
+const AuthContext = createContext(null);
+
+export function AuthContextProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user: any) => {
+      setUser(user);
+      console.log("user: ", children);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const signIn = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+
+        setUser(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+  };
+
+  const signUp = (email, password) => {
+    createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setUser(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+  };
+
+  const logOut = () => {
+    signOut(auth)
+      .then(() => {})
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, signIn, signUp, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuthContext() {
+  const ctx = useContext(AuthContext);
+
+  return ctx;
+}
