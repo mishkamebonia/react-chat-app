ea3630e45c50785842ce15ad10e93e53b2437dc7
mishkamebonia <mishkas.mebonia@gmail.com>
2024-01-28 22:22:46 +0400

    refactored main page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,13 @@
-import NavBar from "../components/NavBar";
-import { TextField, Box, Grid, Button } from "@mui/material";
+import NavBar from "../components/content/NavBar";
+import { Grid } from "@mui/material";
 import { useAuthContext } from "../providers/auth";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import SideBar from "../components/content/SideBar";
+import Content from "../components/content/Content";
 
 const MainPage = () => {
   const { user } = useAuthContext();
   const [users, setUsers] = useState([]);
-  const navigate = useNavigate();
 
   // console.log(user);
 
@@ -22,11 +22,6 @@ const MainPage = () => {
     }
   }, [user]);
 
-  const onClickUserBtn = (id: string) => {
-    navigate(`${id}`);
-    console.log(id);
-  };
-
   return (
     <div>
       <NavBar />
@@ -37,52 +32,8 @@ const MainPage = () => {
         justifyContent="space-between"
         alignItems="stretch"
       >
-        <Grid item xs={3} sx={{ bgcolor: "#B9B4C7" }}>
-          {users.map((perUser) => {
-            if (user.email !== perUser.email) {
-              return (
-                <Box key={perUser.uid} sx={{ p: 4 }}>
-                  <Button
-                    fullWidth
-                    variant="contained"
-                    onClick={() => onClickUserBtn(perUser.uid)}
-                  >
-                    {perUser.email}
-                  </Button>
-                </Box>
-              );
-            }
-          })}
-        </Grid>
-        <Grid item xs={9} sx={{ bgcolor: "#fff" }}>
-          <Grid
-            container
-            direction="column"
-            justifyContent="space-between"
-            sx={{ p: 4, height: "100%" }}
-          >
-            <Grid item xs={5}>
-              text
-            </Grid>
-            <Grid item>
-              <Grid container spacing={2}>
-                <Grid item xs={10}>
-                  <TextField
-                    size="small"
-                    fullWidth
-                    placeholder="Type a message"
-                    variant="outlined"
-                  />
-                </Grid>
-                <Grid item xs={2}>
-                  <Button fullWidth color="primary" variant="contained">
-                    Send
-                  </Button>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
+        <SideBar users={users} />
+        <Content />
       </Grid>
     </div>
   );
